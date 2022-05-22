6581dbd452ed1eec4ecb138245fab31402e2f81e
Taner Saydam <tanersaydam@gmail.com>
2022-05-22 21:22:58 +0300

    Add project files.

---
+++ Commands/MyCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace InsertGuid
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+
+            if (selection.HasValue)
+            {
+                string name = selection.Value.ToString();
+                var guid = Guid.NewGuid().ToString();
+                docView.TextBuffer.Replace(selection.Value, guid);
+            }
+        }
+    }
+}
